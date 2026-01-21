d49c144cb13708d01322b1789a037951aa2a78f4
EzequielFerreiraC <ezequiel34324@gmail.com>
2026-01-21 13:41:36 -0300

    OK: Add: DtoeMapper

---
+++ demo/src/main/java/br/com/ezequiel/demo/service/BoletoService.java
@@ -0,0 +1,39 @@
+package br.com.ezequiel.demo.service;
+
+import br.com.ezequiel.demo.dto.BoletoDto;
+import br.com.ezequiel.demo.entity.BoletoEntity;
+import br.com.ezequiel.demo.entity.enums.SituacaoBoleto;
+import br.com.ezequiel.demo.mapper.BoletoMapper;
+import br.com.ezequiel.demo.repository.BoletoRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class BoletoService {
+    private final BoletoRepository boletoRepository;
+
+    public BoletoService(BoletoRepository boletoRepository) {
+        this.boletoRepository = boletoRepository;
+    }
+
+    public BoletoDto salvar(String codigoBarras) {
+        var boletoOptional = boletoRepository.findByCodigoBarras(codigoBarras);
+
+        if(boletoOptional.isPresent()) {
+            throw new RuntimeException("Já existe uma solicitação de pagamento para esse boleto");
+        }
+
+        var boletoEntity = BoletoEntity.builder()
+                .codigoBarras(codigoBarras)
+                .situacaoBoleto(SituacaoBoleto.INICIALIZADO)
+                .dataCriacao(LocalDateTime.now())
+                .dataAtualizacao(LocalDateTime.now())
+                .build();
+        boletoRepository.save(boletoEntity);
+
+        return BoletoMapper.toDto(boletoEntity);
+    }
+
+
+}
